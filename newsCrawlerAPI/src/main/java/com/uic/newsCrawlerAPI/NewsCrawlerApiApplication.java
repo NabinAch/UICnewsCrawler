8ef5addb060f02f9a9bdcdca3bd53ea8e3a09639
Nabin Acharya <nabinach@live.com>
2018-09-30 12:00:34 -0400

    Created new Spring Boot RESTful api

---
+++ newsCrawlerAPI/src/main/java/com/uic/newsCrawlerAPI/NewsCrawlerApiApplication.java
@@ -0,0 +1,12 @@
+package com.uic.newsCrawlerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsCrawlerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsCrawlerApiApplication.class, args);
+	}
+}
---
+++ newsCrawlerAPI/src/test/java/com/uic/newsCrawlerAPI/NewsCrawlerApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uic.newsCrawlerAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewsCrawlerApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
