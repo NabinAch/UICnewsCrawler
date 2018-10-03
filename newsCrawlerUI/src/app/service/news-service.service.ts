7a9802810a1cfc29f26c3d8473cfecf897686b62
Nabin Acharya <nabinach@live.com>
2018-10-03 08:13:14 -0700

    read api URL from enviroment configuration

--- newsCrawlerUI/src/app/service/news-service.service.ts
+++ newsCrawlerUI/src/app/service/news-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NewsEntity } from '../Entity/newsEntity';
 import { Observable } from '../../../node_modules/rxjs';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,10 @@ export class NewsServiceService {
 
   apiAddress: string;
   constructor(private _http: HttpClient) {
-    this.apiAddress='http://localhost:8080/news?date=';
-   }
+    this.apiAddress = environment.apiUrl;
+  }
 
-  getNews(searchDate:any): Observable<Array<NewsEntity>>{
-    return this._http.get<Array<NewsEntity>>(this.apiAddress+searchDate);
+  getNews(searchDate: any): Observable<Array<NewsEntity>> {
+    return this._http.get<Array<NewsEntity>>(this.apiAddress + searchDate);
   }
 }
